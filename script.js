2f8b59b405eee2792b33b4c328de64409ffae4c5
alisami0111 <saminagri0111@gmail.com>
2024-07-09 14:28:16 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function openMenu(){
+  const sidebar = document.querySelector('.sidebar')
+  sidebar.style.display = "flex"
+
+
+}
+
+function closeMenu(){
+  const sidebar = document.querySelector('.sidebar')
+  sidebar.style.display = "none"
+
+
+}
